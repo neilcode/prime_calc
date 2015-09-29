177b292b6115460c48330b7f071593073f0e1a13
Neil Lambert <neil@crushpath.com>
2015-09-29 13:22:23 -0700

    Add the multiplication table calculator and specs

---
+++ calculator.rb
@@ -0,0 +1,30 @@
+class Calculator
+
+	attr_reader :number_set
+
+	def initialize(data)
+		data = data.map(&:to_i) if data[0].is_a?(String)
+		@number_set = data
+	end
+
+	def multiples
+		# memoize this data so calling it more than once
+		# doesn't require re-running calculations on static data.
+		@multiples ||= create_multiplication_table
+	end
+
+	private
+
+	def create_multiplication_table
+		multiples = []
+
+		number_set.each do |number|
+			row = number_set.each_with_object([]) { |n, arr| arr << (n * number) }
+			multiples << row
+		end
+
+		multiples
+	end
+
+
+end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require_relative '../calculator.rb'
+
+describe Calculator do
+	describe 'initialization' do
+		
+		it 'accepts an array of integers' do 
+			expect { @calc = Calculator.new([1,2,3]) }.to_not raise_error
+		end
+
+		it 'accepts an array of stringified integers and converts them' do
+			@calc = Calculator.new(%w(1 2 3))
+			expect(@calc.number_set).to eq [1,2,3]
+		end
+	
+	end
+
+	describe '#multiples' do
+		before do
+			@calc = Calculator.new([2,4,6,8,10])
+			@output = @calc.multiples
+		end
+		
+		it 'returns an array of integer arrays' do
+			expect(@output.first).to be_a(Array)
+			expect(@output.first.first).to be_a(Integer)
+		end
+
+		it 'multiples[x][y] is the product of number_set[x] * number_set[y]' do
+			@number_set = @calc.number_set
+			
+			@number_set.each_with_index do |num, i|
+				other_num = @number_set.sample
+				other_num_index = @number_set.index(other_num)
+
+				expect(@output[i][other_num_index]).to eq (num * other_num)
+			end
+		end
+	end
+end
